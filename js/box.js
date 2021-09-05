341e2875ddfd3d3e26b710b4aa88772c1bd0a670
kimisak <isak7263@gmail.com>
2021-09-06 00:25:14 +0900

    title, subTitle animation

--- js/box.js
+++ js/box.js
@@ -1,14 +1,34 @@
 function boxLoad(index, max) {
-    console.log("boxLoad - index: "+ index + ", max : "+ max);
-
+    //console.log("boxLoad - index: "+ index + ", max : "+ max);
+    
+    // box
+    box = document.querySelectorAll(".box")[index];
+    // console.log(box);
     // 첫 번째 박스 로드
     if (index == 0 || index == max) {
-        console.log("첫 번째 box load")
+        index = 0;
         
-        var bottomLine = document.querySelector(".bottomLine");
+        // console.log("첫 번째 box load")
+
+        // Title
+        var title = box.querySelector("#title");
+        title.removeAttribute("id");
+        void title.clientWidth;
+        title.setAttribute("id","title");
+        
+        // subTitle
+        var subTitle = box.querySelector(".subTitle");
+        subTitle.removeAttribute("class");
+        void subTitle.offsetWidth; // 브라우저에서 강제 랜더링 시키기
+        subTitle.setAttribute("class", "subTitle");
+        
+        // 바닥 라인
+        var bottomLine = box.querySelector(".bottomLine");
         bottomLine.removeAttribute("class");
         void bottomLine.offsetWidth; // 브라우저에서 강제 랜더링 시키기
         bottomLine.setAttribute("class", "bottomLine");
+        
+        
         
     }
 }
