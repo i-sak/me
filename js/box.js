2d67447b0ff3acaa848690db846fd1536ef29eb6
isaac <isaac7263@naver.com>
2021-09-07 11:08:42 +0900

    js modify

--- js/box.js
+++ js/box.js
@@ -16,22 +16,11 @@ function boxLoad(index, max) {
         void title.clientWidth;
         title.setAttribute("id","title");
         
-        // subTitle
-        var subTitle = box.querySelectorAll(".subTitle");
-        subTitle[0].removeAttribute("class");
-        subTitle[1].removeAttribute("class");
-        void subTitle[0].offsetWidth; // 브라우저에서 강제 랜더링 시키기
-        void subTitle[1].offsetWidth; // 브라우저에서 강제 랜더링 시키기
-        subTitle[0].setAttribute("class", "subTitle");
-        subTitle[1].setAttribute("class", "subTitle");
-        
         // 바닥 라인
         var bottomLine = box.querySelector(".bottomLine");
         bottomLine.removeAttribute("class");
         void bottomLine.offsetWidth; // 브라우저에서 강제 랜더링 시키기
         bottomLine.setAttribute("class", "bottomLine");
-        
-        
-        
+
     }
 }
