4e305ef556b9f976717ea289fc430f34b7e0179a
isaac <isaac7263@naver.com>
2021-09-14 13:04:15 +0900

    sp up1

--- js/box2.js
+++ js/box2.js
@@ -3,30 +3,6 @@ function clickBox2Aticle(idx) {
     console.log("clickBox2Aticle idx : " + idx)
     // box cover
     visibledBox2hiddenDivParent();
-/*
-    var hiddenDiv = document.querySelector(".hiddenDiv");
-    hiddenDiv.style.visibility = 'hidden'
-
-    var box2hiddenArticle = document.querySelector("#box2hiddenArticle"+idx);
-    box2hiddenArticle.style.visibility = 'visible'
-  */  
-    
-
-
-    /*
-    switch(idx)
-    {
-        case 1: 
-            
-        break;
-        case 2: 
-        break;
-        case 3: 
-        break;
-        case 4: 
-        break;
-    }
-    */
 }
 // on
 function visibledBox2hiddenDivParent () {
@@ -37,6 +13,4 @@ function visibledBox2hiddenDivParent () {
 function closeBox2hiddenDivParent() {
     var box2hiddenDivParent = document.querySelector("div#box2hiddenDivParent");
     box2hiddenDivParent.style.animation = 'visibilityOff 0.3s forwards'
-    
-    
 }
